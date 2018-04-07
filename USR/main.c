8095270e286898dbdb1c27d8d6e6e3c320285c31
Aero <Aerotic@users.noreply.github.com>
2018-04-07 22:15:58 +0800

    Add usart2.c and print via USART2

--- USR/main.c
+++ USR/main.c
@@ -6,7 +6,17 @@
 
 #include "stm32f4xx.h"
 #include "stm32f4xx_conf.h"
+#include "usart2.h"
 
 int main(void)
 {	
+	Usart2_Config();
+//	Usart2_SendChar(0xff);
+//	Usart2_SendChar(0xff);
+//	Usart2_SendChar(0xff);
+//	Usart2_SendChar(0xff);
+//	Usart2_SendChar(0xff);
+	printf("Successfully print string via serial");
+	while(1){
+	}
 }
